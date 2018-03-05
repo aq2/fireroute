a881bd3356b1533a20430253201521a2729db5f6
angelo <aq211@exeter.ac.uk>
2018-03-05 02:42:04 +0000

    sofa UI

--- src/pages/viz/parallel/ParallelViz.vue
+++ src/pages/viz/parallel/ParallelViz.vue
@@ -1,7 +1,13 @@
 <template lang="pug">
   
+#payge  
   #gfx
     svg(id='mySvg')
+  #list
+    h1 candidates
+    .candList
+      .cand(v-for='(name,i) in candNames' )
+        p(:ID="'cand' + i") {{i}} {{name}} 
 
 </template>
 
@@ -15,6 +21,13 @@ name: 'scratch',
 
 mounted() {
   this.main()
+},
+
+data() {
+  return {
+    dimNames: [],
+    candNames: []
+  }
 },
 
 computed: {
@@ -32,24 +45,23 @@ computed: {
 methods: {
   // todo - could split into sep fns
   main() {
-    //
+    const windowW = window.innerWidth
+    const windowH = window.innerHeight
+
     // set size of svg 
-    const svgHeight = 800, svgWidth = 500
+    const svgHeight = (windowH-100)*0.95, svgWidth = windowW*0.68
     const margin = 60  // ie 2*30
+    const chartHeight = svgHeight - margin, chartWidth = svgWidth - margin
     
     const svg = d3.select('#mySvg')
                   .attr('height', svgHeight)
                   .attr('width', svgWidth)
-
-    const chartHeight = svgHeight - margin
-    const chartWidth = svgWidth - margin
 
     // drawable area of chart, inside the margin
     const chartGrp = svg.append('g')
                         .attr('class', 'chart')
                         .attr('transform', this.myXY(margin/2, margin/2))
     
-    //
     // setup a yAxis for each dimension 
     let dims = [], dimScores = []
     let mins = [], maxs = []
@@ -59,7 +71,7 @@ methods: {
     // fetch store data and set scales and axes for each dim
     Object.entries(this.dimData).forEach(([k, dimD]) => {
       if (dimD.crit) {
-        dims.push(dimD.dimName)
+        this.dimNames.push(dimD.dimName)
         dimScores.push(dimD.scores)
 
         const min = dimD.stats.min
@@ -78,18 +90,15 @@ methods: {
         dimColors.push(this.randomColor()())
       }
     })
-    // console.log('dC', dimColors)
 
-    //
     // setup shared x scale and axis
     const xScale = d3.scalePoint()
-                      .domain(dims)
+                      .domain(this.dimNames)
                       .range([0, chartWidth])
 
     const xAxis = d3.axisBottom(xScale)
                     .tickPadding(5)
     
-    // 
     // put axes in a group, and shift a bit to make margin
     const axesGrp = chartGrp.append('g')
                             .attr('class', 'axes')
@@ -102,19 +111,17 @@ methods: {
     const yAxesGrp = axesGrp.append('g')
                     .attr('class', 'yAxes')
 
-    //
     // add a new group for circles
     const circlesGrp = chartGrp.append('g')
                               .attr('class', 'circles')
 
-    //
     // for each dim, add axis and plot circles
-    dims.forEach((dim, j) => {
+    this.dimNames.forEach((dim, j) => {
       // add an axis
       yAxesGrp.append('g')
               .attr('class', 'yAxis' + j)
               .call(yAxes[j])
-              .attr('transform', this.myXY(chartWidth * j / (dims.length - 1), 0))
+              .attr('transform', this.myXY(chartWidth * j / (this.dimNames.length - 1), 0))
 
       // plot points on axis for dimension scores
       circlesGrp.selectAll('circle.' + dim)                        
@@ -123,7 +130,7 @@ methods: {
           // .each((d, i) =>  console.log('d' + i +' is', d))
           .append('circle')
           .attr('class', () => dim)                      
-          .attr('cx', () => chartWidth*j/(dims.length - 1))
+          .attr('cx', () => chartWidth*j/(this.dimNames.length - 1))
           .attr('cy', (d) => yScales[j](d))
           .attr('r', '4')
           .attr('fill', dimColors[j])
@@ -132,14 +139,14 @@ methods: {
     //
     // need to build up candScores from dimScores
     // probably some fancy map method, but i'll do it noobly
-    let candScores = [], candNames = []
+    let candScores = [] //, candNames = []
     let candXs = [], candYs = []   // not actually using candXs, but could...
     let selectedCands = this.selectedCands
     let candColors = []
 
     // get the candIDs for naming
     selectedCands.forEach((c) => {
-      candNames.push(this.candiData[c].candID)
+      this.candNames.push(this.candiData[c].candID)
       candScores.push([])
       candColors.push(this.randomColor()())
     })
@@ -166,7 +173,7 @@ methods: {
     // ok, what about the lines?
     // path generator to build line
     const line = d3.line()
-                    .x((d, i) => (chartWidth*i/(dims.length - 1)))
+                    .x((d, i) => (chartWidth*i/(this.dimNames.length - 1)))
                     .y(d => d)
                     // .curve(d3.curveOrdinal)
                     // .curve(d3.curveNatural)
@@ -186,9 +193,6 @@ methods: {
               .attr('stroke', candColors[c])
               .attr('stroke-width', '2px')
     }) 
-
-    console.log('rc', this.randomColor()())
-
   }, // end main
 
   myXY(x, y) {
@@ -249,12 +253,18 @@ methods: {
 
 <style lang="stylus" scoped>
 
+#payge
+  margin 1rem
+  display flex
+  // background blue
+
 #gfx
-  margin 1 rem
+  // margin 1rem
   // background green
 
 svg
-  background-color blue
+  background-color $g3
+  // position fixed
   // border 2px dotted black
 
 // these don't seem to work
@@ -272,6 +282,11 @@ g.grp2 path
 g.tick
   stroke red
   fill red
+
+#list
+  background $g5
+  padding 1rem
+  overflow-y auto
 
 
 </style>
